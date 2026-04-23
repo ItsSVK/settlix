e007035c203be825b0a84fd55a7e91d9a2db0fe8
Shouvik Mohanta <connectshouvik@gmail.com>
2026-04-23 21:48:33 +0530

    chore: remove Anchor program and associated configuration files

--- program/programs/settlix/src/instructions/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod close_vault;
-pub mod distribute;
-pub mod initialize_vault;
-pub mod update_recipients;
-pub use close_vault::*;
-pub use distribute::*;
-pub use initialize_vault::*;
-pub use update_recipients::*;
